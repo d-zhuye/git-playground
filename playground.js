04c870fca64e4754de641ed8877c0339a3f30bd6
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2025-01-17 22:33:28 -0800

    Add example func for merge conflict

--- playground.js
+++ playground.js
@@ -8,3 +8,8 @@ for (let i = 0; i < 5; i++) {
 // This is an example text;
 
 //Added during testing branch
+
+function makeAMergeConflict() {
+  console.log("This is a merge conflict.");
+  console.log("Delete this in refactor branch.");
+}
